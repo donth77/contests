23fa74d4fabab4c4499ba90562bfdb64d17cdef4
donth77 <donth77@gmail.com>
2019-04-06 02:24:08 -0700

    Update Solution.java

--- google-codejam/2019/qualification/Problem1/Solution.java
+++ google-codejam/2019/qualification/Problem1/Solution.java
@@ -1,6 +1,6 @@
-import java.util. * ;
-import java.lang. * ;
-import java.io. * ;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Solution {
 	static double power(double d, int n) {
